21d3e5999913994f7daeb6c0c753f679593ea860
Max Krivosheia <maxkr1203@gmail.com>
2021-06-02 18:43:48 +0300

    finish

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,1 +1,6 @@
-console.log('hi');
+let nav_btn = document.getElementById("nav_btn");
+let nav_list = document.getElementById("nav_list");
+
+nav_btn.addEventListener("click", () => {
+    nav_list.classList.toggle("nav__list--active");
+});
